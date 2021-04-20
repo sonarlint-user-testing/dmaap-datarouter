c82ee03ebe3e9399e8428190e020d11a61975d75
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-04-20 10:35:24 +0200

    Fix SQL Injection

--- datarouter-prov/src/main/java/org/onap/dmaap/datarouter/provisioning/store/StatisticsStore.java
+++ datarouter-prov/src/main/java/org/onap/dmaap/datarouter/provisioning/store/StatisticsStore.java
@@ -54,9 +54,11 @@ public class StatisticsStore {
   private PreparedStatement makePreparedStatement(String feedid, Connection conn) throws SQLException, ParseException {
     String sql;
     eventlogger.info("Generating sql query to get Statistics resultset. ");
-    sql =  "SELECT * FROM LOG_RECORDS WHERE id=" + feedid;
+    sql =  "SELECT * FROM LOG_RECORDS WHERE id = ? ";
     eventlogger.debug("SQL Query for Statistics resultset. " + sql);
-    return conn.prepareStatement(sql);
+    PreparedStatement ret = conn.prepareStatement(sql);
+    ret.setString(1, feedid);
+    return ret;
   }
 
 
